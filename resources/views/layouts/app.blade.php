e80c12c8f50a36931aaa809bdfb3749cdd27bc89
philip-white <phlwhite5@gmail.com>
2020-06-08 18:48:16 -0300

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
         <title>{{config('app.name', 'almusiclessons')}}</title>
 
          <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
@@ -20,17 +24,6 @@
         <link rel="stylesheet" href="/css/myOwnCss.css">
 
 
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'almusiclessons') }}</title>
-
-    <!-- Scripts -->
-
-    <!-- Fonts -->
-
-    <!-- Styles -->
 </head>
 <body>
     @include('inc.navbar')
